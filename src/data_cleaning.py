cce2c0eabfbd4da3024549349627069e10fb1394
PARTHIBAN-007 <157013976+PARTHIBAN-007@users.noreply.github.com>
2025-04-28 19:19:43 +0530

    Data Cleaning

---
+++ src/data_cleaning.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import yaml
+from datetime import date
+from sklearn.preprocessing import  LabelEncoder
+
+class DataCleaning:
+    def __init__(self):
+
+        self.encoder = LabelEncoder()
+        
+    def clean_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        
+        # Drop irrelevant columns
+        df = df.drop("case_id",axis=1)
+
+        today = date.today()
+        current_year = today.year
+        df['company_age'] = current_year-df["yr_of_estab"]
+        df = df.drop(columns="yr_of_estab")
+
+        numeric_features = df.select_dtypes(exclude="object").columns
+        categoric_features = df.select_dtypes(include="object").columns
+            
+        for columns in categoric_features:
+            df[columns] = self.encoder.fit_transform(df[columns])
+                      
+        return df
+    
+    
